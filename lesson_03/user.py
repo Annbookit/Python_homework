81a85967c50ed2068b554d0d144bdc0c80963bbf
Annbookit <sebast57945@gmail.com>
2025-03-05 22:02:36 +0500

    lesson3 add

---
+++ lesson_03/user.py
@@ -0,0 +1,20 @@
+class User:
+  
+
+    def __init__(self, name_1, name_2):
+        self.username_1 = name_1
+        self.username_2 = name_2 
+    
+    def Name_1(self):
+       print("Мое имя:", self.username_1)
+
+    def Name_2(self):
+       print("Моя фамилия:", self.username_2)
+
+    def Name(self):
+        print("Меня зовут:", self.username_1, self.username_2)
+
+
+
+
+
