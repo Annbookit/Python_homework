97b568c744a7760980c16073c8f4d3227fe9acc9
Annbookit <sebast57945@gmail.com>
2025-05-12 18:10:31 +0500

    lesson6knew add

--- lesson_06k/test_02_calc.py
+++ lesson_06k/test_02_calc.py
@@ -1,4 +1,3 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,23 +5,30 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.maximize_window()
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+def calculator():
+   driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+   driver.maximize_window()
+   driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+   driver.implicitly_wait(20)
+   
+   driver.find_element(By.CSS_SELECTOR, "#delay").clear()
+   driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
 
-driver.find_element(By.CSS_SELECTOR, "#delay").clear()
-driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
+   driver.find_element(By.XPATH, "//span[text()='7']").click()
+   driver.find_element(By.XPATH, "//span[text()='+']").click()
+   driver.find_element(By.XPATH, "//span[text()='8']").click()
+   driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
+   waiter = WebDriverWait(driver, 45)
+   waiter.until_not(
+        EC.visibility_of(driver.find_element(By.CSS_SELECTOR, "#spinner"))
+    )
 
-waiter = WebDriverWait(driver, 45)
-waiter.until_not(
-    EC.visibility_of(driver.find_element(By.CSS_SELECTOR, "#spinner"))
-)
+   res = driver.find_element(By.CSS_SELECTOR, ".screen").text
 
-res = driver.find_element(By.CSS_SELECTOR, ".screen").text
+   assert int(res) == 15
 
-assert int(res) == 15
+   driver.quit()
+
+def test_calc():
+  calculator()
